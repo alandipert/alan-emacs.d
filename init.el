999344ca564ddb872f9f61802333dfea6b8a2e6f
Alan Dipert <alan@dipert.org>
2011-04-28 00:59:41 -0400

    blow out org-mode git, change location of tr repo

--- init.el
+++ init.el
@@ -80,18 +80,6 @@
         (:name clojure-mode :type elpa)
         (:name durendal :after (lambda () (durendal-enable)))
 
-        (:name org-mode
-               :type git
-               :url "git://orgmode.org/org-mode.git"
-               :info "doc"
-               :build `,(mapcar
-                         (lambda (target)
-                           (concat "make " target " EMACS=" el-get-emacs))
-                         '("clean" "all"))
-               :load-path ("lisp" "contrib/lisp")
-               :autoloads nil
-               :features org-install)
-
         (:name slime
                :after (lambda ()
                         (setq slime-protocol-version 'ignore)))
